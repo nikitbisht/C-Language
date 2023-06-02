e779fd61e3a8405f385b6d7e205b5474aac131d1
nikitbisht <135353429+nikitbisht@users.noreply.github.com>
2023-06-02 22:14:28 +0530

    reverse_string.c

---
+++ Strings/reverse_string.c
@@ -0,0 +1,30 @@
+/*
+Qn:- Wrinte a C program to reverse a string without using inbuild function ?
+Input:- 
+Enter the String:- Nikit Singh Bisht
+Output:-
+Reverse String:- thsiB hgniS tikiN
+*/
+
+#include<stdio.h>
+int main(){
+	char s1[200],s2;
+	int i=0;
+	puts("Enter the String:- ");
+	gets(s1);						// Also used scanf("%[^\n]s",s1);
+	//Reach the s[i] to the end character of string
+	while(s1[i]!='\0')
+		i++;
+	i=i-1;
+	//here i is lenth of sting
+	int j=0;
+	for(j=0;j<=i;j++){
+		s2=s1[j];
+		s1[j]=s1[i];
+		s1[i]=s2;
+		i--;
+	}
+	printf("Reverse String:- \n");
+	puts(s1);
+	return 0;
+}
